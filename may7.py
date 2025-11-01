e230e2c5d5558fe586d11e484505a033f55413ca
Ибрагим <agkacev2007@gmail.com>
2025-11-01 15:02:49 +0300

    fix commit

--- may7.py
+++ may7.py
@@ -1,6 +1,7 @@
-a=input()
-b=0
-while a!="стоп" or a!="хватит" or a!="достаточно":
-    b=b+1
-    a=input()
-print(b)
+n=[1,2,3,4,5]
+def int_(n):
+    summa=0
+    for i in range(len(n)):
+        n[i]=n[i]*1.1
+    return n
+print(int_(n))
